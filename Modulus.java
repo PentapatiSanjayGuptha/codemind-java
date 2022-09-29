a9fff0d1ebc9607c574ea62d03bccfe83efc50f2
PentapatiSanjayGuptha <112751508+PentapatiSanjayGuptha@users.noreply.github.com>
2022-09-29 22:39:29 +0530

    import java.util.Scanner;class Main{ public static void main(String arge[]) { Scanner sc=new Scanner(System.in); int m,n,mod; m=sc.nextInt(); n=sc.nextInt(); mod=(int)m%n; System.out.println(mod); }}

---
+++ Modulus.java
@@ -0,0 +1,1 @@
+import java.util.Scanner;class Main{ public static void main(String arge[]) { Scanner sc=new Scanner(System.in); int m,n,mod; m=sc.nextInt(); n=sc.nextInt(); mod=(int)m%n; System.out.println(mod); }}
