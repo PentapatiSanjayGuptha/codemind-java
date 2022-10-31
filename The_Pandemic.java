36cb21efca4cdf24cdc5a036002a356fb305f7f4
Sanjay Guptha <112751508+PentapatiSanjayGuptha@users.noreply.github.com>
2022-10-31 21:27:14 +0530

    import java.util.*; class biggest {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         if(n<3)          System.out.println(n-1);         else         System.out.println("2" );     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class biggest
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n<3)
+         System.out.println(n-1);
+        else
+        System.out.println("2" );
+    }
+}
