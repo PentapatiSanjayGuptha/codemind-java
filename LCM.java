d4439d70045106426b89d569cb60302c345e24af
Sanjay Guptha <112751508+PentapatiSanjayGuptha@users.noreply.github.com>
2022-11-02 19:47:15 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,max=0,i,lcm=0;         a=sc.nextInt();         b=sc.nextInt();         if(a>b)         max=a;         else         max=b;         for(i=max;;i++)         {             if(i%a==0 && i%b==0)             {                 lcm=i;                 break;             }         }         System.out.println(lcm);     } }

--- LCM.java
+++ LCM.java
@@ -4,19 +4,19 @@ class main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int a,b,max=0,lcm=0,i;
+        int a,b,max=0,i,lcm=0;
         a=sc.nextInt();
         b=sc.nextInt();
         if(a>b)
         max=a;
         else
         max=b;
-        for(i=max;i>0;i++)
+        for(i=max;;i++)
         {
             if(i%a==0 && i%b==0)
             {
-            lcm=i;
-            break;
+                lcm=i;
+                break;
             }
         }
         System.out.println(lcm);
