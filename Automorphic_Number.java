b48acfdabb441872b74673f68d81aa4b9709323c
Sanjay Guptha <112751508+PentapatiSanjayGuptha@users.noreply.github.com>
2022-11-05 13:45:47 +0530

    import java.util.*; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,d,s,r;         n=sc.nextInt();         d=(int)Math.log10(n)+1;         s=n*n;         r=s%(int)Math.pow(10,d);         if(r==n)         System.out.print("Automorphic Number");         else         System.out.print("Not an Automorphic Number");     } }

--- Automorphic_Number.java
+++ Automorphic_Number.java
@@ -1,18 +1,17 @@
-import java.util.Scanner; 
-class sample
+import java.util.*;
+class Main
 {
- public static void main(String args[]) 
-{ 
-Scanner sc=new Scanner(System.in); 
-int n,d,sq,r; 
-n=sc.nextInt(); 
-d=(int)Math.log10(n)+1; 
-sq=n*n;
- r=sq%(int)Math.pow(10,d); 
- if(r==n)
- System.out.println("Automorphic Number");
- else 
-System.out.println("Not an Automorphic Number");
- sc.close();
-}
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,d,s,r;
+        n=sc.nextInt();
+        d=(int)Math.log10(n)+1;
+        s=n*n;
+        r=s%(int)Math.pow(10,d);
+        if(r==n)
+        System.out.print("Automorphic Number");
+        else
+        System.out.print("Not an Automorphic Number");
+    }
 }
