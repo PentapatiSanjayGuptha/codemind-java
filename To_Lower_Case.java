0fb97e5e31f73d27bb4c13c39710096bf94c512e
Sanjay Guptha <112751508+PentapatiSanjayGuptha@users.noreply.github.com>
2023-01-06 18:47:52 +0530

    import java.util.*; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s;         s=sc.nextLine();         s=s.toLowerCase();         System.out.println(s);     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s;
+        s=sc.nextLine();
+        s=s.toLowerCase();
+        System.out.println(s);
+    }
+}
